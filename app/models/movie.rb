7824f75b85335f9ff9adc143cc72eceaa9a3be87
thomas <tom.angotti11@gmail.com>
2023-02-06 14:52:16 -0700

    commit tree 51b226b372680ff3b1b2838745156e0654220387

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,50 @@
 class Movie < ActiveRecord::Base
+    
+    def self.find_all_movies_by_year(year)
+        Movie.where(year: year)
+    end
+    
+    def self.create_with_title(title)
+        Movie.create(title: title, release_date: nil, director: nil, lead: nil, in_theaters: nil)
+    end
 
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(value)
+        Movie.find_by(value)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where('release_date > 2002')
+    end
+
+    def update_with_attributes(value)
+        self.update(value)
+    end
+
+    def self.update_all_titles(value)
+        Movie.update_all(title: value)
+    end
+
+    def self.delete_by_id(id)
+        Movie.find(id).destroy
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 end
